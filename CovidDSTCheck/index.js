af827d1f5d57c81b1c7d20b62f2c217283818e43
Jim Bumgardner <jbum@jbum.com>
2023-03-02 15:50:35 -0800

    Removed unused Crons.

--- CovidDSTCheck/index.js
+++
@@ -1,8 +0,0 @@
-const { slackBotChatPost } = require('../common/slackBot');
-const dstNotifyChannel = 'C02J16U50KE'; // #jbum-testing
-const appName = 'CovidDSTCheck';
-
-module.exports = async function (context, myTimer) {
-  let dtString = (new Date()).toLocaleDateString() + ' ' + (new Date()).toLocaleTimeString();
-  await slackBotChatPost(dstNotifyChannel,`*${appName} firing at ${dtString}`);
-};
