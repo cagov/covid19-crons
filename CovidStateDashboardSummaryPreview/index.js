97f777548bae600e02439de47b48bec82a974df7
James Logan <jamesstanleylogan@gmail.com>
2023-07-26 15:45:27 -0700

    add summary preview

--- CovidStateDashboardSummaryPreview/index.js
+++ CovidStateDashboardSummaryPreview/index.js
@@ -1,28 +1,20 @@
-const { doCovidStateDashboardSummary } = require('./worker');
+const { doCovidStateDashboardSummary } = require('../CovidStateDashboardSummary/worker');
 const { slackBotChatPost, slackBotReportError, slackBotReplyPost, slackBotReactionAdd } = require('../common/slackBot');
 const { isIdleDay } = require('../common/timeOffCheck');
-const notifyChannel = 'C01AA1ZB05B'; // #covid19-state-dash
-const debugChannel = 'C01DBP67MSQ'; // #testingbot
+const debugChannel = true || process.env.debug ? 'C02J16U50KE' : 'C01DBP67MSQ' //#jbum-testing vs #testingbot
 
 module.exports = async function (context, myTimer) {
   const appName = context.executionContext.functionName;
   let slackPostTS = null;
   try {
-    slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every Thursday @ 7:45am)`)).json()).ts;
+    slackPostTS = (await (await slackBotChatPost(debugChannel,`${appName} (Every Wednesday @ 3:45pm)`)).json()).ts;
 
     if (isIdleDay({weekends_off:true, holidays_off:true})) {
       await slackBotReplyPost(debugChannel, slackPostTS,`${appName} snoozed (weekend or holiday)`);
       await slackBotReactionAdd(debugChannel, slackPostTS, 'zzz');
     } else {
 
-      const TreeRunResults = await doCovidStateDashboardSummary(false);
-
-      if(TreeRunResults.Pull_Request_URL) {
-        const prMessage = `Biweekly stats summary ready\n${TreeRunResults.Pull_Request_URL}`;
-        await slackBotReplyPost(debugChannel, slackPostTS, prMessage);
-        await slackBotReactionAdd(debugChannel, slackPostTS, 'package');
-        await slackBotChatPost(notifyChannel, prMessage);
-      }
+      const TreeRunResults = await doCovidStateDashboardSummary(true);
 
       await slackBotReplyPost(debugChannel, slackPostTS,`${appName} finished`);
       await slackBotReactionAdd(debugChannel, slackPostTS, 'white_check_mark');
